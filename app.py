f03e44e25cef250a7589e68b856bd2bd7f6afdbb
Inyeop <seeker1204@naver.com>
2022-04-17 15:12:15 +0900

    2nd

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from importlib.resources import path
+import os, pickle
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    return render_template('index.html')
+
+@app.route('/mnist', methods=['GET', 'POST'])
+def mnist() :
+    if request.method == 'GET' :
+        return render_template('mnistform.html')
+    else :
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__) + '/upload/' + f.filename
+        f.save(path)
+        img = Image.open(path).convert("L")
+        img = np.resize(img,(1,784))
+        # img = 255 - (img)
+        mpath = os.path.dirname(__file__)+'/model1.pickle'
+        with open(mpath,'rb') as f:
+            model = pickle.load(f)
+        pred = model.predict(img)
+        return render_template('mnistresult.html',data=pred)
+
+if __name__ == '__main__' :
+    app.run(debug = True)
